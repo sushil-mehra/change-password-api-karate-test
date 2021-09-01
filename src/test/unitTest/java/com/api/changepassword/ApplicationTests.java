5e8570c3f50531345cd1dd71075bd9463cee38a7
sushil-mehra <>
2021-09-02 02:36:37 +0530

    update to PUT unit test

--- src/test/unitTest/java/com/api/changepassword/ApplicationTests.java
+++ src/test/unitTest/java/com/api/changepassword/ApplicationTests.java
@@ -1,15 +1,19 @@
 package com.api.changepassword;
 
 import com.api.changepassword.controller.UserController;
-import com.api.changepassword.model.User;
-import org.junit.Assert;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,20 +34,28 @@ public class ApplicationTests {
 
     @Test
     public void contextLoads() throws Exception {
-        Assert.assertNotNull(userController);
+        assertNotNull(userController);
     }
 
     @Test
-    public void testUpdatePut() {
+    public void testPutCall() throws JSONException {
         String userId = "user1";
-        String newPwd = "Password@123456";
-        User user = restTemplate.getForObject(getRootUrl() + "/users/" + userId, User.class);
-        user.setUsername(userId);
-        user.setPwd(newPwd);
+        String oldPwd = "BRandeomma245@##3@";
+        String newPwd = "Pasd3@BRandeomma245";
+        String reqJson = "{\"oldPwd\":\"" + oldPwd + "\",\"newPwd\":\"" + newPwd + "\"}";
 
-        restTemplate.put(getRootUrl() + "/users/" + userId, user);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(reqJson, headers);
 
-        User updatedUser = restTemplate.getForObject(getRootUrl() + "/users/" + userId, User.class);
-        Assert.assertNotNull(updatedUser);
+        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/v1/updatePassword/users/" + userId,
+                HttpMethod.PUT, entity, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(200, response.getStatusCodeValue());
+
+        JSONObject json = new JSONObject(response.getBody());
+        assertEquals("Password has been updated successfully", json.get("message"));
+        assertEquals(userId, json.get("userId"));
     }
 }
